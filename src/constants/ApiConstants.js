8cf0b589c85c1c85d9c58050f0c74888a81b0335
Igor <gorbov18@gmail.com>
2018-07-13 14:36:59 +0300

    https

--- src/constants/ApiConstants.js
+++ src/constants/ApiConstants.js
@@ -1,8 +1,8 @@
 export const SERVER_USERS_URL =
-  'http://5b3884f3e1f26d0014569eb7.mockapi.io/user';
+  'https://5b3884f3e1f26d0014569eb7.mockapi.io/user';
 
 export const SERVER_ALBUMS_URL =
-  'http://www.mocky.io/v2/5b48846f2f00005f0f4818f7';
+  'https://www.mocky.io/v2/5b48846f2f00005f0f4818f7';
 
 export const SERVER_CATEGORIES_URL =
-  'http://www.mocky.io/v2/5b4749a63200001041301f45';
+  'https://www.mocky.io/v2/5b4749a63200001041301f45';
